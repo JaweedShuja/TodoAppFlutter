2db53cfd997024b965556261012d241485d241aa
JaweedShuja <56365178+JaweedShuja@users.noreply.github.com>
2020-02-17 22:39:12 +0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/Home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
